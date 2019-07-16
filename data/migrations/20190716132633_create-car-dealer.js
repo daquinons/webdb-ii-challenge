aa79bfcc2d93efa7973613c3dd7c1ae133f7d63b
David Quiñones <daquinons@users.noreply.github.com>
2019-07-16 13:46:43 +0200

    Add migration and seeds

--- data/migrations/20190716132633_create-car-dealer.js
+++ data/migrations/20190716132633_create-car-dealer.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', table => {
+    table.increments();
+    table
+      .text('vin', 128)
+      .unique()
+      .notNullable();
+    table.text('make', 128).notNullable();
+    table.text('model', 128).notNullable();
+    table.float('mileage').notNullable();
+    table.text('transmission_type', 128);
+    table.text('status', 128);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
