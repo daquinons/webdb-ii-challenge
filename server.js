6d074275abf8ed2ccaae3b1c645628db84302b0b
David Quiñones <daquinons@users.noreply.github.com>
2019-07-16 13:57:38 +0200

    Setup server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/', (req, res) => res.json({ message: 'Server up and running' }));
+
+module.exports = server;
